9baf972855cee09d8af36b52c1154ee3799c0482
Ryan-Caruth <ryan.caruth2@gmail.com>
2022-05-28 13:59:05 -0600

    Reading current time in console

--- clock.js
+++ clock.js
@@ -1,16 +1,27 @@
 const currentTime = () => {
     let currentTime = new Date()
-    console.log(currentTime)
+    // console.log(currentTime)
     let hours = currentTime.getHours()
-    console.log(`The current hour is: ${hours}`)
+    // console.log(`The current hour is: ${hours}`)
     let minutes = currentTime.getMinutes()
-    console.log(`The current minutes are: ${minutes}`)
+    // console.log(`The current minutes are: ${minutes}`)
     let seconds = currentTime.getSeconds()
-    console.log(`The current seconds are: ${seconds}`)
+    // console.log(`The current seconds are: ${seconds}`)
+    am_pm = 'AM'
+
+    if (hours >= 12) {
+        am_pm = 'PM'
+        console.log(am_pm)
+    }
+    //Concatenate variables to produce current time string
+    let time = (`${hours}:${minutes}:${seconds} ${am_pm}`)
+    console.log(time);
 }
 
 
-var timevariable = 9
+
+
+
 
 
 
