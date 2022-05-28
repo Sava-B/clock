387ab81d582e8d8a5d4bb043f1ae235f4f8e479a
Ryan-Caruth <ryan.caruth2@gmail.com>
2022-05-28 13:29:20 -0600

    Defined the variables to extract indivitual times

---
+++ clock.js
@@ -0,0 +1,26 @@
+const currentTime = () => {
+    let currentTime = new Date()
+    console.log(currentTime)
+    let hours = currentTime.getHours()
+    console.log(`The current hour is: ${hours}`)
+    let minutes = currentTime.getMinutes()
+    console.log(`The current minutes are: ${minutes}`)
+    let seconds = currentTime.getSeconds()
+    console.log(`The current seconds are: ${seconds}`)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+currentTime()
