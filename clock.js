26cf84e280bb5672e3411a99538290440c5ebabc
Ryan-Caruth <ryan.caruth2@gmail.com>
2022-05-28 14:47:37 -0600

    Displayed clock in browser console

--- clock.js
+++ clock.js
@@ -1,28 +1,20 @@
-const currentTime = () => {
-    let currentTime = new Date()
-    // console.log(currentTime)
-    let hours = currentTime.getHours()
-    // console.log(`The current hour is: ${hours}`)
-    let minutes = currentTime.getMinutes()
-    // console.log(`The current minutes are: ${minutes}`)
-    let seconds = currentTime.getSeconds()
-    // console.log(`The current seconds are: ${seconds}`)
-    //Concatenate variables to produce current time string
-    let time = (`${hours}:${minutes}:${seconds} ${am_pm}`)
-    console.log(time);
-}
+let currentTime = new Date();
+// console.log(currentTime)
+let hours = currentTime.getHours();
+// console.log(`The current hour is: ${hours}`)
+let minutes = currentTime.getMinutes();
+// console.log(`The current minutes are: ${minutes}`)
+let seconds = currentTime.getSeconds();
 
 const amOrPm = () => {
-    let am_pm = 'AM'
+  let am_pm = "AM";
 
-    if (hours >= 12) {
-        am_pm = 'PM'
-        console.log(am_pm)
-    }
+  if (hours >= 12) {
+    am_pm = "PM";
+    console.log(am_pm);
+  }
+  let time = `${hours}:${minutes}:${seconds} ${am_pm}`;
+  console.log(time);
+};
 
-
-
-}
-
-
-currentTime()
+amOrPm();
